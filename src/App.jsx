5deb3c4e157f0462e0d5f62c8b461f3ffc026484
rajakharmachi <raja.kharmachi@sesame.com.tn>
2024-02-26 10:39:47 +0100

    nn

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
-import * as React from 'react';
-
-const list = [
-  { title: "react", url: "http://www.redux.org" },
-];
+import React from 'react';
 
 function App() {
+  const list = [
+    { objectID: 1, url: 'https://example.com/1', title: 'Item 1', author: 'Author 1', num_comments: 10, points: 20 },
+    { objectID: 2, url: 'https://example.com/2', title: 'Item 2', author: 'Author 2', num_comments: 15, points: 25 },
+    { objectID: 3, url: 'https://example.com/3', title: 'Item 3', author: 'Author 3', num_comments: 20, points: 30 }
+  ];
+
   return (
     <div>
-      <h1>React app</h1>
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
-        alt="React Logo"
-        className="photo"
-      />
       <ul>
         {list.map(function (item) {
           return (
-            <li key={item.title}>
+            <li key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
               </span>
