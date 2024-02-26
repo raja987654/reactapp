2e5188be0d721a23d12b62d54d35bbbd5a113278
rajakharmachi <raja.kharmachi@sesame.com.tn>
2024-02-26 10:51:12 +0100

    nn

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,22 @@ import React from 'react';
 
 function App() {
   const list = [
-    { objectID: 1, url: 'https://example.com/1', title: 'Item 1', author: 'Author 1', num_comments: 10, points: 20 },
-    { objectID: 2, url: 'https://example.com/2', title: 'Item 2', author: 'Author 2', num_comments: 15, points: 25 },
-    { objectID: 3, url: 'https://example.com/3', title: 'Item 3', author: 'Author 3', num_comments: 20, points: 30 }
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1
+    }
   ];
 
   return (
